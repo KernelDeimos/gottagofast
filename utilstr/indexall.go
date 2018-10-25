9b5079c6508b22e0fc8fe5299b5d9721efdf1d7f
Eric Dubé <eric.alex.dube@gmail.com>
2018-10-24 23:29:44 -0400

    Add utilstr package

---
+++ utilstr/indexall.go
@@ -0,0 +1,30 @@
+package utilstr
+
+import (
+	"strings"
+)
+
+// IndexAll reports the index of every occurrance of a substring. When two
+// substrings overlap, the index of the first instance will be taken.
+func IndexAll(
+	input, substr string,
+) []int {
+	indices := []int{}
+	var startAt int
+	// While an instance still exists after the start location
+	for {
+		// Get index of substring if one exists
+		index := strings.Index(input[startAt:], substr)
+		if index == -1 {
+			break
+		}
+
+		// Add index as startAt+index (index on input string)
+		indices = append(indices, startAt+index)
+		// Increement startAt to be the position immediately after this instance
+		// of the substring
+		startAt += index + len(substr)
+	}
+
+	return indices
+}
